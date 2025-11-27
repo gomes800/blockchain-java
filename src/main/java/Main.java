8021f36e1ae67d0bda783c355b45cb573bfdb604
gom-trab <jgomesleite8@gmail.com>
2025-11-27 09:54:06 -0300

    starting bockchain

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+
+    public static void main(String[] args) {
+
+        blockchain.add(new Block("Hi im the first block", "0"));
+        blockchain.add(new Block("Yo im the second block", blockchain.get(blockchain.size() - 1).hash));
+        blockchain.add(new Block("Hello im the third block", blockchain.get(blockchain.size() - 1).hash));
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+    }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal.");
+                return false;
+            }
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal.");
+                return false;
+            }
+        }
+        return true;
+    }
+}
